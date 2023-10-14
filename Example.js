8ff5960e7dc2bc53b92cf8b3d0535b297c94fe36
cenim <enimchristopher@yahoo.com>
2023-10-14 09:50:24 +0000

    switched example on app

--- Example.js
+++ Example.js
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
-import { View,Text,Image } from "react-native";
+import { View, Text, Image } from "react-native";
 import CountryPicker from "./App";
 
 
 const Example = () => {
     const [showPicker, setShowPicker] = useState(true);
+    const [countryData, setCountryData] = useState(null)
 
 
     return (
         <View style={{ flexDirection: 'column', flex: 1 }}>
-             <Text style={{alignSelf:'center',marginTop:100}} onPress={()=>{
+            <Text style={{ alignSelf: 'center', marginTop: 100 }} onPress={() => {
                 setShowPicker(true)
             }}>OPEN</Text>
-              <CountryPicker
-                visible={showPicker}  
+            <CountryPicker
+                visible={showPicker}
                 onClose={() => setShowPicker(false)}
                 onCountrySelected={(data) => {
+                    setCountryData(data)
                     console.log(JSON.stringify(data))
                 }}
             />
+            {
+                countryData && <View style={{ flexDirection: 'column' }}>
+                    <Text></Text>
+                    <Text>{JSON.stringify(countryData)}</Text>
+
+                    <Image source={countryData.flag} style={{width:50,height:50}}/>
+                </View>
+            }
+
 
 
         </View>
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
-import Example from './Example';
+import App from './App';
 import {name as appName} from './app.json';
 
 
-AppRegistry.registerComponent(appName, () => Example);
+AppRegistry.registerComponent(appName, () => App);
